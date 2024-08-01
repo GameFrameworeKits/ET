121a1c0560d1503c953d982ef285b917c9ffe28a
zhangxinsheng <zhangxinsheng@weile.com>
2024-08-01 13:23:55 +0800

    添加注释

--- Unity/Assets/Scripts/Core/World/Module/EventSystem/EventSystem.cs
+++ Unity/Assets/Scripts/Core/World/Module/EventSystem/EventSystem.cs
@@ -151,6 +151,9 @@ namespace ET
         // 既然Invoke跟函数一样，那么为什么不使用函数呢? 因为有时候不方便直接调用，比如Config加载，在客户端跟服务端加载方式不一样。比如TimerComponent需要根据Id分发
         // 注意，不要把Invoke当函数使用，这样会造成代码可读性降低，能用函数不要用Invoke
         // publish是事件，抛出去可以没人订阅，调用者跟被调用者属于两个模块，比如任务系统需要知道道具使用的信息，则订阅道具使用事件
+        // Publish区分SceneType，Invoke没有
+        // Publish没有分发作用，Invoke带有分发作用
+        // Publish1对多，Invoke1对1
         public void Invoke<A>(long type, A args) where A: struct
         {
             if (!this.allInvokers.TryGetValue(typeof(A), out var invokeHandlers))
