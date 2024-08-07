b4826231c7ef70bcde8deea8a59282b25b55cede
zhangxinsheng <zhangxinsheng@weile.com>
2024-08-07 17:31:25 +0800

    添加场景管理模块

---
+++ Unity/Assets/Plugins/EGF/Runtime/Scene/Event/UnloadSceneFailureEventArgs.cs
@@ -0,0 +1,60 @@
+﻿using ET;
+
+namespace EGF
+{
+    /// <summary>
+    /// 卸载场景失败事件。
+    /// </summary>
+    public sealed class UnloadSceneFailureEventArgs : IReference
+    {
+        /// <summary>
+        /// 初始化卸载场景失败事件的新实例。
+        /// </summary>
+        public UnloadSceneFailureEventArgs()
+        {
+            SceneAssetName = null;
+            UserData = null;
+        }
+
+        /// <summary>
+        /// 获取场景资源名称。
+        /// </summary>
+        public string SceneAssetName
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取用户自定义数据。
+        /// </summary>
+        public object UserData
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 创建卸载场景失败事件。
+        /// </summary>
+        /// <param name="sceneAssetName">场景资源名称。</param>
+        /// <param name="userData">用户自定义数据。</param>
+        /// <returns>创建的卸载场景失败事件。</returns>
+        public static UnloadSceneFailureEventArgs Create(string sceneAssetName, object userData)
+        {
+            UnloadSceneFailureEventArgs unloadSceneFailureEventArgs = ReferencePool.Acquire<UnloadSceneFailureEventArgs>();
+            unloadSceneFailureEventArgs.SceneAssetName = sceneAssetName;
+            unloadSceneFailureEventArgs.UserData = userData;
+            return unloadSceneFailureEventArgs;
+        }
+
+        /// <summary>
+        /// 清理卸载场景失败事件。
+        /// </summary>
+        public void Clear()
+        {
+            SceneAssetName = null;
+            UserData = null;
+        }
+    }
+}
